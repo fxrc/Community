725e8090cf184127236860f4a977797dc964746a
fxrc <1027192650@qq.com>
2022-03-31 16:54:46 +0800

    first commit

---
+++ src/test/java/com/example/community/CommunityApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+class CommunityApplicationTests {
+
+
+
+}
